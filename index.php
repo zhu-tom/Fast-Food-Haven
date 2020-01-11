de69801a75f6de83f264d245d2a7d7ebd925ca3f
ItIsAMe <51206392+ItIsAMe@users.noreply.github.com>
2020-01-11 14:39:02 -0800

    Update index.php

--- index.php
+++ index.php
@@ -14,9 +14,6 @@
         <a class="navbar-brand" href="#">Fast Food Haven</a>
     </nav>
 
-  <div id = "cards" class='container'>
-  </div>
-
     <div class='container'>
         <div class='form-row'>
             <div class='col'>
@@ -50,44 +47,8 @@
     </div>
     <div class='container'>
 
-        <div class="row row-cols-1 row-cols-md-3">
-            <div class="col mb-4">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-4">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
-            </div>
-            </div>
-            <div class="col mb-4">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                </div>
-            </div>
-            </div>
-            <div class="col mb-4">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
-            </div>
-            </div>
-        </div>
+      <div id = "cards" class='container'>
+      </div>
     </div>
 </body>
 </html>
