cd0ce79867c86378feee2aa15c4baea042edc5fe
zhu-tom <zhutom01@gmail.com>
2020-01-11 14:01:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,14 +11,6 @@
     <nav class="navbar navbar-light bg-light">
         <a class="navbar-brand" href="#">Fast Food Haven</a>
     </nav>
-
-  <div class="row row-cols-1 row-cols-md-3">
-    <div class="col mb-4">
-      <div class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     <div class='container'>
         <div class='row'>
             <div class='col'>
@@ -36,6 +28,14 @@
         </div>
       </div>
     </div>
+
+  <div class="row row-cols-1 row-cols-md-3">
+    <div class="col mb-4">
+      <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     <div class="col mb-4">
       <div class="card">
         <img src="..." class="card-img-top" alt="...">
