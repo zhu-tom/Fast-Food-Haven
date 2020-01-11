a62f3047df515959728cf822f8d8a9fcfe128714
zhu-tom <zhutom01@gmail.com>
2020-01-11 16:11:34 -0500

    ewopj

--- index.js
+++ index.js
@@ -5,26 +5,44 @@ $(document).ready(() => {
         success: (response) => {
             let data = response;
             let lines = data.split("\n");
+            lines = lines.slice(1);
+            console.log(lines);
 
-            for (let i = 1; i<lines.length;i++)
-            {
+            for (let i = 0; i < lines.length-1; i++) {
+                if (i % 3 == 0) {
+                    $('#cards').append('<div class="card-deck"></div>');
+                }
                 let line = lines[i].split(":");
-                let title = line[0];
-                let content = line[1];
 
-                let newDiv = document.createElement("div");
-                newDiv.className = "col mb-4";
-                $("#cards").append(newDiv);
-
-                let newDiv2 = document.createElement("div");
-                newDiv2.className = "card-body";
-                newDiv.appendChild(newDiv2);
-
-                let newTitle = document.createElement("h5");
-                newTitle.innerHTML = title;
-                console.log(newTitle.innerHTML);
-                newDiv2.appendChild(newTitle);
+                let card = '<div class="card mb-4">\
+                                <img src="..." class="card-img-top" alt="...">\
+                                <div class="card-body">\
+                                    <h5 class="card-title">'+ line[0] + '</h5>\
+                                    <p class="card-text">' + line[1] +'</p>\
+                                </div>\
+                            </div>'
+                $('#cards').children().last().append(card);
             }
+
+            // for (let i = 1; i<lines.length;i++)
+            // {
+            //     let line = lines[i].split(":");
+            //     let title = line[0];
+            //     let content = line[1];
+
+            //     let newDiv = document.createElement("div");
+            //     newDiv.className = "col mb-4";
+            //     $("#cards").append(newDiv);
+
+            //     let newDiv2 = document.createElement("div");
+            //     newDiv2.className = "card-body";
+            //     newDiv.appendChild(newDiv2);
+
+            //     let newTitle = document.createElement("h5");
+            //     newTitle.innerHTML = title;
+            //     console.log(newTitle.innerHTML);
+            //     newDiv2.appendChild(newTitle);
+            // }
         }
     });
     $(document).on('click', '#price', (event) => {
--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <a class="navbar-brand" href="#">Fast Food Haven</a>
     </nav>
 
-  <div id = "cards" class = "col mb-4">
+  <div id = "cards" class='container'>
   </div>
 
     <div class='container'>
@@ -56,13 +56,13 @@
 
         <div class="row row-cols-1 row-cols-md-3">
             <div class="col mb-4">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
                 </div>
-            </div>
             </div>
             <div class="col mb-4">
             <div class="card">
