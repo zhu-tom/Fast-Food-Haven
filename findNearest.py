9377add05f600eaca06bf83893ce7a0ca0f7ab4e
zhu-tom <zhutom01@gmail.com>
2020-01-12 08:48:05 -0500

    ferewefwfe

--- findNearest.py
+++ findNearest.py
@@ -3,8 +3,9 @@ import requests
 key = 'AIzaSyAO3n76BQcfMed4_XroJUVZEGINZbFzCQ4'
 
 def findNearest(lat, lon, rest):
-    url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lon}&keyword={rest}&type=point_of_interest&rankby=distance&opennow=true&key={key}'
+    url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lon}&keyword={rest}&type=point_of_interest&radius=50000&opennow=true&key={key}'
     response = requests.get(url)
+    print(url)
     if response.json()['status'] != "ZERO_RESULTS":
         return findDist(lat, lon, response.json()['results'][0]['place_id'])
     else:
